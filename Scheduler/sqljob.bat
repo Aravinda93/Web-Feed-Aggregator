f70c3691011a468776143cf509f5a6db7f6b1454
Aravinda <bavb93@gmail.com>
2019-07-02 17:45:20 +0200

    Update sqljob.bat

--- Scheduler/sqljob.bat
+++ Scheduler/sqljob.bat
@@ -1,4 +1,4 @@
-sqlcmd -S BVBALIGA\SQLExpress -U sa -P Fifa@2014 -d Data_And_Web_SS19 -Q " EXEC DELETE_OLD_RECORDS "
+sqlcmd -S Server_Name -U sa -P mypass -d Data_And_Web_SS19 -Q " EXEC DELETE_OLD_RECORDS "
 
 echo Data Older Than 30 are deleted from the table
 
